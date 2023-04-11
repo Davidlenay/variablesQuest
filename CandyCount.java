3c28e191b9d43cbc232c752bb56b452d0982e6e2
DAVIDLNAY <davidlenay@gmail.com>
2023-04-11 11:26:19 +0200

    first commit

---
+++ CandyCount.java
@@ -0,0 +1,17 @@
+public class CandyCount {
+    public static void main(String[] args){      
+        System.out.println("CandyCount");
+
+        double money = 12.4;
+        double price = 1.2;
+        int candies = 0;
+
+        if (money > 0 && price > 0) {
+            while (money >= price) {
+                candies++;
+                money -= price;
+            }
+        }
+        System.out.println(candies);
+    }
+} 
